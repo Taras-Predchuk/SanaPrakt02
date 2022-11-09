e2f299e3eb8c1096356bdebca2e818cb15ee4671
Taras Predchuk <ipzm222_ptv@student.ztu.edu.ua>
2022-11-09 21:35:46 +0200

    changed 3 line in branch text

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Довільний текст 1");
+Console.WriteLine("Довільний текст 2");
+Console.WriteLine("Довільний текст 3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
